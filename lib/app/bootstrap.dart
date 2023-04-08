863c4591b318ab26c0882f5509311df2227d6eba
ricardodalarme <ricardodalarme@outlook.com>
2023-04-08 18:15:17 -0300

    feat: add firebase services

--- lib/app/bootstrap.dart
+++ lib/app/bootstrap.dart
@@ -1,18 +1,25 @@
 import 'dart:async';
-import 'dart:developer';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_starter/app/bloc_observer.dart';
+import 'package:flutter_bloc_starter/firebase_options.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   Bloc.observer = AppBlocObserver();
-  FlutterError.onError = (details) {
-    log(details.exceptionAsString(), stackTrace: details.stack);
-  };
 
   await runZonedGuarded(
     () async => runApp(await builder()),
-    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
+    FirebaseCrashlytics.instance.recordError,
   );
 }

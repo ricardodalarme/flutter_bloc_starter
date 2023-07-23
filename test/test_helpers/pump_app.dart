551141f312286f7fca060417dc268f7d71298e0b
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-07-23 15:01:57 -0300

    test: fix breaking test

--- test/test_helpers/pump_app.dart
+++ test/test_helpers/pump_app.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_starter/l10n/l10n.dart';
+import 'package:flutter_bloc_starter/repositories/authentication_repository.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) async {
     await pumpWidget(
-      MaterialApp(
-        home: widget,
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
+      RepositoryProvider(
+        create: (context) => AuthenticationRepository(),
+        child: MaterialApp(
+          home: widget,
+          localizationsDelegates: const [
+            AppLocalizations.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+        ),
       ),
     );
   }

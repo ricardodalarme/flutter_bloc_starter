954f39f7245541a93053cb1f9a7410651a1c3004
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-08-20 12:31:48 -0300

    fix(tests): inject the user repository

--- test/test_helpers/pump_app.dart
+++ test/test_helpers/pump_app.dart
@@ -4,12 +4,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_starter/l10n/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:user_repository/user_repository.dart';
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) async {
     await pumpWidget(
-      RepositoryProvider(
-        create: (context) => AuthenticationRepository(),
+      MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider(
+            create: (context) => AuthenticationRepository(),
+          ),
+          RepositoryProvider(
+            create: (context) => UserRepository(),
+          ),
+        ],
         child: MaterialApp(
           home: widget,
           localizationsDelegates: const [

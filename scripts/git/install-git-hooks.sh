c5b21c831167cd57090267520beb3d6642efb77f
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-09-08 00:22:38 -0300

    sdas

--- scripts/git/install-git-hooks.sh
+++ scripts/git/install-git-hooks.sh
@@ -2,14 +2,17 @@
 set -e
 
 SELF=$(basename "$0")
-HOOKS_DIR=$(dirname "$PWD"/"$0")
-GIT_COMMON_DIR=$(git rev-parse --git-common-dir) # finds the directory containing the `hooks` directory
+HOOKS_DIR=$(dirname "$0")  # Use "$0" directly to get the script's directory
+GIT_COMMON_DIR=$(git rev-parse --show-toplevel)/.git  # Find the top-level .git directory
 
-shopt -s globstar nullglob
+if [ ! -d "$GIT_COMMON_DIR/hooks" ]; then
+    mkdir -p "$GIT_COMMON_DIR/hooks"
+fi
+
 for F in "$HOOKS_DIR"/*; do
-    HOOK_NAME=$(basename "$-F")
+    HOOK_NAME=$(basename "$F")
     if [ "$SELF" != "$HOOK_NAME" ] && [ -x "$F" ]; then
-        echo "installing $F as $GIT_COMMON_DIR/hooks/$HOOK_NAME"
-        ln -sf "$F" "$GIT_COMMON_DIR"/hooks/"$HOOK_NAME"
+        echo "Installing $F as $GIT_COMMON_DIR/hooks/$HOOK_NAME"
+        ln -sf "$F" "$GIT_COMMON_DIR/hooks/$HOOK_NAME"
     fi
 done

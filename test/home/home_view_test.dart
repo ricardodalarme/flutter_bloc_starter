9510f6ac9020206ebf273858d1d3831b87da0671
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-09-08 00:24:38 -0300

    wip

--- test/home/home_view_test.dart
+++ test/home/home_view_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import '../test_helpers/pump_app.dart';
 
 void main() {
-  group('HomeView', () {
+  group('HssomeView', () {
     testWidgets('renders view', (tester) async {
       await tester.pumpApp(const HomeView());
       await tester.pump();

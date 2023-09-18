6358cae72938a336ba50b17ab65524114f3e4578
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-09-17 22:38:44 -0300

    feat: add hydrated_bloc

--- lib/features/app/bootstrap.dart
+++ lib/features/app/bootstrap.dart
@@ -2,10 +2,12 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_starter/features/app/bloc_observer.dart';
 import 'package:flutter_bloc_starter/firebase_options.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,9 +16,16 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
-
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
   Bloc.observer = AppBlocObserver();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationCacheDirectory(),
+  );
+
+  if (kDebugMode) {
+    await HydratedBloc.storage.clear();
+  }
 
   runApp(await builder());
 }

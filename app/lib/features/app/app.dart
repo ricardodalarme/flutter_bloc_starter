8f37d4201528fd9dbd710a2423502ddcd4a3b08a
ricardodalarme <ricardo.dalarme@profusion.mobi>
2023-11-20 16:00:42 -0300

    refactor(router): use dependency injection for AppRouter

--- app/lib/features/app/app.dart
+++ app/lib/features/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:common/common.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -5,15 +6,14 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:quickstart_flutter_bloc/features/app/bloc/app_bloc.dart';
 import 'package:quickstart_flutter_bloc/features/app/observers/router_observer.dart';
 import 'package:quickstart_flutter_bloc/l10n/translations.g.dart';
-import 'package:quickstart_flutter_bloc/routes/app_router.dart';
 
 class App extends StatelessWidget {
-  App({super.key});
-
-  final _appRouter = AppRouter();
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final appRouter = AppInjector.instance.get<RootStackRouter>();
+
     return BlocProvider.value(
       value: AppInjector.instance.get<AppBloc>(),
       child: TranslationProvider(
@@ -22,7 +22,7 @@ class App extends StatelessWidget {
             onGenerateTitle: (context) => context.l10n.common.appName,
             theme: ThemeData.light(useMaterial3: true),
             darkTheme: ThemeData.dark(useMaterial3: true),
-            routerConfig: _appRouter.config(
+            routerConfig: appRouter.config(
               navigatorObservers: () => [
                 AppRouterObserver(),
               ],
--- app/lib/features/app/di/app_injection_module.dart
+++ app/lib/features/app/di/app_injection_module.dart
@@ -1,16 +1,23 @@
 import 'dart:async';
 
 import 'package:authentication_repository/authentication_repository.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:common/common.dart';
 import 'package:quickstart_flutter_bloc/features/app/bloc/app_bloc.dart';
+import 'package:quickstart_flutter_bloc/routes/app_router.dart';
 import 'package:user_repository/user_repository.dart';
 
 class AppInjectionModule extends InjectionModule {
   @override
   FutureOr<void> registerDependencies({required Injector injector}) {
+    injector.registerLazySingleton<RootStackRouter>(
+      AppRouter.new,
+    );
+
     injector.registerLazySingleton<AuthenticationRepository>(
       AuthenticationRepository.new,
     );
+
     injector.registerLazySingleton<UserRepository>(
       UserRepository.new,
     );

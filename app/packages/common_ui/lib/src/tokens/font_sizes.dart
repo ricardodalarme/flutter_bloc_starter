0ec018f9e0a7fcf9023401292ba7a9846db52e4f
ricardodalarme <ricardo.dalarme@profusion.mobi>
2024-05-04 14:38:58 -0300

    feat(theme): add font sizes tokens

---
+++ app/packages/common_ui/lib/src/tokens/font_sizes.dart
@@ -0,0 +1,9 @@
+abstract final class FontSizes {
+  static const double xsmall = 10;
+  static const double small = 12;
+  static const double medium = 14;
+  static const double large = 16;
+  static const double xlarge = 20;
+  static const double xxlarge = 24;
+  static const double xxxlarge = 30;
+}

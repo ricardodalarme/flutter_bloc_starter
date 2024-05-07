7f7e6044a07c973895b0ab99ce067d0247186f1e
ricardodalarme <ricardo.dalarme@profusion.mobi>
2024-05-07 00:32:23 -0300

    feat(common_ui): add info snack bar type

--- app/packages/common_ui/lib/src/widgets/base_snack_bar.dart
+++ app/packages/common_ui/lib/src/widgets/base_snack_bar.dart
@@ -1,16 +1,21 @@
-import 'package:flutter/material.dart' show Colors, ScaffoldMessenger, SnackBar;
-import 'package:flutter/widgets.dart';
+import 'package:common_ui/common_ui.dart';
+import 'package:flutter/material.dart' show ScaffoldMessenger, SnackBar;
 
-enum SnackBarType { success, error }
+enum SnackBarType { info, success, error }
 
 extension ContextSnackBarExtension on BuildContext {
   void showSnackBar({
     required String message,
-    required SnackBarType type,
+    SnackBarType type = SnackBarType.info,
   }) {
+    final backgroudColor = switch (type) {
+      SnackBarType.info => colors.inverseSurface,
+      SnackBarType.success => Colors.green400,
+      SnackBarType.error => Colors.red400,
+    };
     final baseSnackBar = SnackBar(
       content: Text(message),
-      backgroundColor: type == SnackBarType.success ? Colors.green : Colors.red,
+      backgroundColor: backgroudColor,
     );
     ScaffoldMessenger.of(this).showSnackBar(baseSnackBar);
   }

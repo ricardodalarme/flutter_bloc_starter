83638469d99c2f85a947762b4017409c1c2a1694
ricardodalarme <ricardo.dalarme@profusion.mobi>
2024-06-30 18:50:17 -0300

    chore(android): use fragment activity

--- app/android/app/src/main/kotlin/main/profusion/quickstart_flutter_bloc/MainActivity.kt
+++ app/android/app/src/main/kotlin/main/profusion/quickstart_flutter_bloc/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.profusion.quickstart_flutter_bloc
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
